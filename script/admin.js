2f10940f310023339c6f325da0bb5ddc2b512db5
JCGViljoen <jcgviljone51@gmail.com>
2023-06-08 12:22:17 +0200

    layout started

---
+++ script/admin.js
@@ -0,0 +1,10 @@
+ const postBtn = document.querySelector('#postbtn')
+ const message = document.querySelector('#message')
+ const author = document.querySelector('#author')
+ const result = document.querySelector('#')
+
+ let arry = []
+
+ postBtn.addEventListener('click', (e) =>(
+    
+ ))
